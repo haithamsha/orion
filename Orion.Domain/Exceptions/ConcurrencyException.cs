21fdff9fc42d831bfd20ba5025a0cc704b5f66c5
haithamsha <haithamshaabann@gmail.com>
2025-10-06 12:00:12 +0300

    clean some file and correct projects references

--- Orion.Domain/Exceptions/ConcurrencyException.cs
+++
@@ -1,9 +0,0 @@
-namespace Orion.Domain.Exceptions;
-
-public class ConcurrencyException : Exception
-{
-    public ConcurrencyException(Guid aggregateId, int expectedVersion, int actualVersion)
-        : base($"Concurrency conflict for aggregate {aggregateId}. Expected version {expectedVersion}, but was {actualVersion}.")
-    {
-    }
-}
